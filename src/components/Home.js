01a4cd4a857c2c0064288f3ca74829bc5163e802
Agnes <agsawicka@gmail.com>
2022-07-19 11:10:28 -0400

    commit tree 5186f6a6d9d56392de61056ed6c54c3eae9b3a03

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,10 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const styleC = {color: 'firebrick'}
+  return (<div id="home">
+    <h1 style={styleC}>Your {name}is a Web Developer from Your {city}</h1>
+    Home</div>);
 }
 
 export default Home;
